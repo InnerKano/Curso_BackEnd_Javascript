d7835ba68bc35177fbd007640a8d6eaf98faa7aa
InnerKano <kevin.cano@utp.edu.co>
2024-02-12 21:39:34 -0500

    Lunes 12/02/2024

---
+++ Vue/new_project/src/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="grid" class="ContenedorGrid">
+    <header>
+      <h1>PROYECTO EN VUE</h1>
+    </header>
+    <nav>
+      <ul>
+        <li><a class="nav" href="#" @click.prevent="$emit('navigate', 'home')">Inicio</a></li>
+        <li><a class="nav" href="#" @click.prevent="$emit('navigate', 'about')">Acerca de...</a></li>
+      </ul>
+    </nav>
+    <main>
+      <componente-01/>
+    </main>
+  </div>
+</template>
+
+
+
+
+
+
+
+<script>
+  import Componente01 from './components/Componente01.vue';
+
+  export default {
+    components: {
+      Componente01
+    }
+    
+  }
+</script>
+
+
+
+
+
+
+
+
+<style scoped>
+  
+  header {
+    grid-area: H;
+    text-align: center;
+    
+  }
+
+  h1 {
+    font-size: 40px;
+    color: green;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+    list-style-type: none;
+  }
+
+  nav{
+    text-align: center;
+    grid-area: N;  
+    
+  }
+  a{
+    text-decoration: none;
+  }
+  ul {
+    list-style-type: none;
+  }
+  li {
+    
+  }
+  .nav{
+    color: black;
+  }
+  
+  main{
+    grid-area: M;
+  }
+  #grid {
+    background-image: linear-gradient(to left,rgba(43, 255, 0, 0.39),rgba(255, 166, 0, 0.589));
+    height: 100%;
+    width: 100%;
+    margin: 0px;
+    display: grid;
+    grid-gap: 10px;
+    grid-template-columns: repeat(4, 25%);
+    grid-template-rows: repeat(5, auto);
+    grid-template-areas:
+      "H H H H"
+      "N N N N"
+      "M M M M"
+      "M M M M"
+      "F F F F";
+  }
+
+
+
+</style>
---
+++ Vue/new_project/src/components/Componente01.vue
@@ -0,0 +1,104 @@
+<template>
+    <div id="grid_comp1" class="ContenedorGrid" style="border-style: dotted;">
+        <h2 id="h2">Componente 01</h2>
+        <section id="S1">        
+            <p>Variable de texto:{{ vtext }}</p>
+            <p>Variable de lista:{{ vlist }}</p>
+            <p>Posicion de una lista: {{ vlist[2] }} </p>
+            <p>Variable de numero:{{ vnum }}</p>
+            <p>Variable de objeto(atrib1):{{ vobj.atrib1 }}</p>
+            <p>Variable de objeto(atrib2):{{ vobj.atrib2 }}</p>
+            <p>Variable de objeto(resta):{{ vobj.atrib2 - vobj.atrib1.value }}</p>
+        </section>  
+        
+        <section id="S2">
+            <p>Modificar el valor del Atributo 1:</p>
+            <button @click="aumentar()">Aumentar atributo 1</button><br/><br/>
+            <button @click="disminuir()">Disminuir atributo 1</button><br/><br/>
+            
+        </section>
+    </div>
+</template>
+
+
+
+
+
+
+<script>
+import {ref} from 'vue';
+
+export default {
+    setup() {
+        let vtext = "texto";
+        let vlist = ["Uno",'dos', 3];
+        let vnum = 5000;
+        let vobj = {
+            atrib1: ref(100),
+            atrib2: 200
+        };
+        //FUNCIONES
+        var aumentar = function(){
+            vobj.atrib1.value++;
+        }
+
+        var disminuir = function(){
+            vobj.atrib1.value--;
+        }
+
+        
+
+        return {
+            vtext,
+            vlist,
+            vnum,
+            vobj,
+            aumentar,
+            disminuir
+        };
+    },
+};
+
+    
+
+</script>
+
+
+
+
+
+
+
+<style>
+    h2 {
+        grid-area: H2;
+        color: black;
+    }
+
+    section{
+        margin: 10px;
+        padding: 10px;
+    }
+
+    #S1{
+        grid-area: S1;
+    }
+
+    #S2{
+        grid-area: S2;
+    }
+
+
+    #grid_comp1 {
+        width: 95%;   
+        margin: 10px; 
+        display: grid;
+        grid-gap: 10px;
+        grid-template-columns: repeat(3, 33.33%);
+        grid-template-rows: repeat(2, auto);
+        grid-template-areas:    "H2 H2 H2"
+                                "S1 S1 S2";  
+    }
+
+
+</style>
