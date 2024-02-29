50387a16ded56f74705bffe6dc36916cd40b55d8
InnerKano <kevin.cano@utp.edu.co>
2024-02-28 21:14:02 -0500

    Me quiero matar

--- Node/Back/src/config.js
+++ Node/Back/src/config.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 // Export an object containing the configuration settings for the application
 
-
 module.exports = {
     // Define the app object with properties related to the application's configuration
     app: {
@@ -9,4 +8,29 @@ module.exports = {
         // This property represents the port number on which the application will run
         port: process.env.PORT || 4000,
     },
+}
+var conexion;
+function conectarBD(){
+    conexion = mysql.createConnection(dbconfig);
+
+    conexion.connect((err) =>{
+        if (err){
+            console.log("Error en la conexion a la BD: "+ err.message)
+            setTimeout(conectarBD, 200);
+        }
+        else{
+            console.log("Conexión a la base de datos correcta");
+        }
+
+    });
+}
+module exports = {
+    app
+    mysql: {
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        passwd: process.env.DB_PASS || '',
+        database: process.env.DB || 'db_censo_sindical'
+    
+    }
 }
--- Node/Back/src/db/mysql.js
+++ Node/Back/src/db/mysql.js
@@ -1,9 +1,31 @@
 const mysql = require('mysql');
 const config = require('../config');
 
+const dbconfig = {
+    host: config.mysql.host,
+    user: config.mysql.user,
+    passwd: config.mysql.passwd,
+    database: config.mysql.database
+}
+
+
+
+module.exports = {
+    obtenerDatos,
+    obtenerRegistro,
+    agregarRegistro,
+    eliminarRegistro
+}
+conectarBD();
 //Traer datos de la tabla
 function obtenerDatos(tabla){
-    return 'Aqui se muestran los registros de mi tabla'
+    //return 'Aqui se muestran los registros de mi tabla'
+
+    return new Promise ((resolve, reject) =>{
+        conexion.query(`SELECT * FROM ${tabla}`, (err, result) =>{
+            return !err ? resolve(result) : reject(console.log("Algo salio mal al realizar la consulta."))
+        });
+    })
 }
 function obtenerRegistro(tabla,id){
     
@@ -13,11 +35,4 @@ function agregarRegistro(tabla, data){
 }
 function eliminarRegistro(tabla, id){
     
-}
-
-module.exports = {
-    obtenerDatos,
-    obtenerRegistro,
-    agregarRegistro,
-    eliminarRegistro
 }
