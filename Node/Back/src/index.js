d97f855c171cb4fe1814485f9704b3cd41219937
Kevin Cano <kevin.cano@utp.edu.co>
2024-02-29 19:54:49 -0500

    Censo sindical, elimate Node

--- Node/Back/src/index.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app') //la instancia dela aplicacion
-
-// // Iniciaalizar el servidor
-// //Con el listen escucha el puerto
-// //La funcion flecha para revisar que este bien
-app.listen(app.get('port'), () => {
-    console.log('Servidor escuchando por el puerto: ', app.get('port'));
-});
